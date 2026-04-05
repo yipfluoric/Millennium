33ef1a356ba65ce5b04c84427c9ad052efb24788
chimera <xeniart@tuta.io>
2026-04-05 19:39:25 -0400

    Just millennium

--- packages/nix/flake.nix
+++ packages/nix/flake.nix
@@ -61,12 +61,8 @@
 
           packages = {
             default             = packages.millennium-steam;
-            millennium-frontend = pkgs.callPackage ./frontend.nix       { inherit millennium-src; };
-            millennium-shims    = pkgs.callPackage ./shims.nix          { inherit millennium-src; };
-            millennium-32       = pkgs.callPackage ./millennium-32.nix  ( millennium-deps );
-            millennium-64       = pkgs.callPackage ./millennium-64.nix  ( millennium-deps );
-            millennium          = pkgs.callPackage ./millennium.nix     ( millennium-deps // { inherit (packages) millennium-32 millennium-64; } );
-            millennium-steam    = pkgs.callPackage ./steam.nix          { inherit (packages) millennium millennium-shims millennium-assets; };
+            millennium          = pkgs.callPackage ./millennium.nix     ( millennium-deps );
+            millennium-steam    = pkgs.callPackage ./steam.nix          { inherit (packages) millennium; };
           };
         in
         packages;

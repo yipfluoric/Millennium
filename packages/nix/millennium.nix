45a402f32e943883e7272604db3f751b1270e4d4
chimera <xeniart@tuta.io>
2026-04-05 19:35:18 -0400

    Remerge
    this is a really dumb idea but maybe

--- packages/nix/millennium.nix
+++ packages/nix/millennium.nix
@@ -1,40 +1,137 @@
 {
+  cmake,
+  ninja,
+  bun,
+  pkg-config,
+  git,
+  cacert,
+
   lib,
-  stdenv,
-  callPackage,
-  fetchFromGitHub,
+  pkgsi686Linux,
 
   inputs,
-  millennium-shims,
-  millennium-assets,
-  millennium-frontend,
-  millennium-32,
-  millennium-64,
+  ...
 }:
-stdenv.mkDerivation {
-  pname = "millennium";
+pkgsi686Linux.stdenv.mkDerivation (finalAttrs: {
+  pname = "millennium-32";
   version = "3.0.0";
 
-  phases = [
-    "installPhase"
-    "fixupPhase"
+  src = inputs.millennium-src;
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+    bun
+    pkg-config
+    git
   ];
 
-  installPhase = ''
-    mkdir -p $out/lib
+  buildInputs = [
+    pkgsi686Linux.gtk3
+    pkgsi686Linux.libgcc
+    pkgsi686Linux.libidn2
+    pkgsi686Linux.libpsl
+    pkgsi686Linux.openssl
+    pkgsi686Linux.brotli
+    pkgsi686Linux.xz
+    pkgsi686Linux.zstd
+    nghttp2
+    cacert
+    libxtst
+    libx11
+  ];
 
-    echo "[Nix Millennium] Merging 32-bit libraries..."
-    cp -v ${millennium-32}/lib/*.so $out/lib/
+  cmakeGenerator = "Ninja";
+  cmakeBuildType = "Release";
+  enableParallelBuilding = true;
 
-    echo "[Nix Millennium] Merging 64-bit libraries..."
-    cp -v ${millennium-64}/lib/*.so $out/lib/
+  cmakeFlags = [
+    "-DGITHUB_ACTION_BUILD=ON"
+    "-DDISTRO_NIX=ON"
+    "-DNIX_FRONTEND=${millennium-frontend}/share/frontend"
+    "-DNIX_SHIMS=${millennium-shims}/share/millennium/shims"
+    "-DCURL_CA_BUNDLE=${cacert}/etc/ssl/certs/ca-bundle.crt"
+    "-DCURL_CA_PATH=${cacert}/etc/ssl/certs"
+  ];
+
+  postPatch = ''
+    mkdir -p deps
+
+    prepare_dep() {
+      local name="$1"
+      local src="$2"
+      echo "[Nix Millennium Build Setup] Preparing dependency: $name"
+      cp -r --no-preserve=mode "$src" "deps/$name"
+      chmod -R u+w "deps/$name"
+    }
+
+    echo "[Nix Millennium Build Setup] Copying all flake inputs to local writable directories"
+    ${
+      let
+        deps = [
+          "zlib"
+          "luajit"
+          "luajson"
+          "websocketpp"
+          "fmt"
+          "json"
+          "minizip"
+          "curl"
+          "incbin"
+          "asio"
+          "abseil"
+          "re2"
+        ];
+      in
+      lib.concatStrings (map (dep: "prepare_dep ${dep} \"${inputs."${dep}-src"}\"\n") deps)
+    }
+
+    echo "[Nix Millennium Build Setup] Initializing Git Repos and adding Dummy Commits"
+    echo "[Nix Millennium Build Setup] Dummy commits are used to determine versions, but flake inputs strip git history, causing issues"
+
+    export HOME=$(pwd)
+
+    git config --global init.defaultBranch main
+    git config --global user.email "nix-build@localhost"
+    git config --global user.name "Nix Build"
+
+    git init
+    git add .
+    git commit -m "Dummy commit for build" > /dev/null 2>&1
+
+    git init deps/luajit
+    git -C deps/luajit add .
+    git -C deps/luajit commit -m "Dummy Commit for Nix Build" > /dev/null 2>&1
+
+    chmod -R u+rwx deps/
+
+    echo "[Nix] Patching root CMakeLists to IGNORE 64-bit source..."
+    sed -i '/add_subdirectory.*src\/hhx64)/s/^/#/' CMakeLists.txt
+
+    echo "[Nix] Patching src/CMakeLists.txt to replace dynamic target reference..."
+    sed -i 's|\$<TARGET_FILE:hhx64>|libmillennium_hhx64.so|g' src/CMakeLists.txt
   '';
 
-  passthru = {
-    assets = millennium-assets;
-    shims = millennium-shims;
-    frontend = millennium-frontend;
-  };
+  buildPhase = ''
+    runHook preBuild
+    cmake --build .
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/lib/
+
+    install -Dm755 src/libmillennium_x86.so                             $out/lib/libmillennium_x86.so
+    install -Dm755 src/boot/linux/libmillennium_bootstrap_x86.so        $out/lib/libmillennium_bootstrap_x86.so
+    install -Dm755 src/libmillennium_luavm_x86                          $out/lib/libmillennium_luavm_x86
+    install -Dm755 src/boot/linux/libmillennium_bootstrap_hhx64.so      $out/lib/libmillennium_bootstrap_hhx64.so
+    install -Dm755 src/libmillennium_hhx64.so                           $out/lib/libmillennium_hhx64.so
+    install -Dm755 src/libmillennium_pvs64                              $out/lib/libmillennium_pvs64
+
+    runHook postInstall
+  '';
 
   meta = {
     homepage = "https://steambrew.app/";
@@ -51,4 +148,4 @@ stdenv.mkDerivation {
       "x86_64-linux"
     ];
   };
-}
+})

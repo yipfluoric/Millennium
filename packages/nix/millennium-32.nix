3fdfd5492ab30d0172daf7a009f652e22ce4d546
chimera <xeniart@tuta.io>
2026-04-06 09:09:14 -0400

    syntax

--- packages/nix/millennium-32.nix
+++ packages/nix/millennium-32.nix
@@ -48,7 +48,7 @@ pkgsi686Linux.stdenv.mkDerivation (finalAttrs: {
   cmakeFlags = [
     "-DGITHUB_ACTION_BUILD=ON"
     "-DDISTRO_NIX=ON"
-    "-DFETCHCONTENT_SOURCE_DIR_SNARE="${inputs.snare-src}"
+    "-DFETCHCONTENT_SOURCE_DIR_SNARE=${inputs.snare-src}"
     "-DCURL_CA_BUNDLE=${cacert}/etc/ssl/certs/ca-bundle.crt"
     "-DCURL_CA_PATH=${cacert}/etc/ssl/certs"
   ];

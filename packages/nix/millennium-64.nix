a94e49ecd9aeee27f8f4cf61b703215d1ac24e9f
chimera <xeniart@tuta.io>
2026-04-06 09:31:45 -0400

    ag

--- packages/nix/millennium-64.nix
+++ packages/nix/millennium-64.nix
@@ -37,7 +37,6 @@ stdenv.mkDerivation (finalAttrs: {
   cmakeFlags = [
     "-DGITHUB_ACTION_BUILD=ON"
     "-DDISTRO_NIX=ON"
-    "-DTARGET_ARCH=x86_64"
     "-DFETCHCONTENT_SOURCE_DIR_SNARE=${inputs.snare-src}"
     "-DCURL_CA_BUNDLE=${cacert}/etc/ssl/certs/ca-bundle.crt"
     "-DCURL_CA_PATH=${cacert}/etc/ssl/certs"
@@ -106,7 +105,6 @@ stdenv.mkDerivation (finalAttrs: {
     mkdir -p $out/lib/
     install -Dm755 src/hhx64/libmillennium_hhx64.so                     $out/lib/libmillennium_hhx64.so
     install -Dm755 src/boot/linux/libmillennium_bootstrap_hhx64.so      $out/lib/libmillennium_bootstrap_hhx64.so
-    install -Dm755 libmillennium_pvs64                                  $out/lib/libmillennium_pvs64
     runHook postInstall
   '';
 
